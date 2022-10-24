916d30dd470bb9ca9d768631fc6611b9c74d7b0e
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-24 16:48:12 -0500

    created constructor/getters/setters for student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,37 @@
-public class Student {
+import java.util.ArrayList;
 
+public class Student {
+    public String name;
+    public long id;
+    public ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double total = 0;
+        for(int grade : grades) {
+            total += grade;
+        }
+        return  total / grades.size();
+    }
 }
